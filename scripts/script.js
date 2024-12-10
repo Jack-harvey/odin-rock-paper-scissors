8fa00fe09666f68a192c9a4aea8648f3c2361f8e
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2024-12-10 18:37:00 +1100

    Add functions for a whole round

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,13 @@
 function computerChoice() {
   let randomNumberFrom1To99 = Math.floor(Math.random() * 100 + 1);
   if (randomNumberFrom1To99 <= 33) {
-    return "Rock";
+    return "rock";
   } else if (randomNumberFrom1To99 >= 67) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
-
-console.log(computerChoice());
 
 function playerChoice() {
   let playerChoice = prompt(
@@ -18,4 +16,51 @@ function playerChoice() {
   return playerChoice.toLowerCase();
 }
 
-console.log(playerChoice());
+function playerWin() {
+  console.log("Congrats, this round is yours!");
+  playerScore++;
+}
+
+function playerLoss() {
+  console.log("That's a point to me, the console!");
+  computerScore++;
+}
+
+function roundEnd() {
+  if (roundNumber <= 4) {
+    return;
+  } else if (playerScore === computerScore) {
+    console.log(`It's a draw. We both scored ${playerScore}`);
+  } else {
+    console.log(
+      `I win. ${roundNumber - playerScore} is lower than ${playerScore}`
+    );
+  }
+}
+
+function calculateWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "draw";
+  } else if (playerChoice === "rock") {
+    return computerChoice === "paper" ? "computer" : "player";
+  } else if (playerChoice === "paper") {
+    return computerChoice === "scissors" ? "computer" : "player";
+  }
+  return computerChoice === "rock" ? "computer" : "player";
+}
+
+let playerChoicex = playerChoice();
+let computerChoicex = computerChoice();
+console.log(playerChoicex);
+console.log(computerChoicex);
+console.log(calculateWinner(playerChoicex, computerChoicex));
+
+// let playerScore = 0;
+// let computerScore = 0;
+// let roundNumber = 0;
+
+// while (roundNumber < 5) {
+//   roundNumber++;
+//   playerChoice();
+//   computerChoice();
+// }
