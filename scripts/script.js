1fa2ea3c05c1f592c99ca860f71bb8486b323577
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2024-12-10 17:35:29 +1100

    Add function to decide computer's choice

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+function computerChoice() {
+  randomNumberFrom1To99 = Math.floor(Math.random() * 100 + 1);
+  if (randomNumberFrom1To99 <= 33) {
+    return "Rock";
+  } else if (randomNumberFrom1To99 >= 67) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+console.log(computerChoice());
