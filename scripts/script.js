a0649f1b338708443595bed13b4e03ba71a29494
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2024-12-10 17:46:48 +1100

    Add function to get player's choice

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function computerChoice() {
-  randomNumberFrom1To99 = Math.floor(Math.random() * 100 + 1);
+  let randomNumberFrom1To99 = Math.floor(Math.random() * 100 + 1);
   if (randomNumberFrom1To99 <= 33) {
     return "Rock";
   } else if (randomNumberFrom1To99 >= 67) {
@@ -10,3 +10,12 @@ function computerChoice() {
 }
 
 console.log(computerChoice());
+
+function playerChoice() {
+  let playerChoice = prompt(
+    "It's time to throw down against me, the console. You've got to pick 'Rock', 'Paper', or 'Scissors'. May the best console win."
+  );
+  return playerChoice.toLowerCase();
+}
+
+console.log(playerChoice());
